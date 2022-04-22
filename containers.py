6fff71fe87aead02acf2f97e8e4a5e7601039965
Mini0nL0ver <ashlee.webby2012@gmail.com>
2022-04-22 20:09:41 +0100

    Added Timeline and Frame classes for viewing frames.

--- containers.py
+++ containers.py
@@ -1,46 +1,47 @@
 from typing import *
+from time import sleep, time
 
 T = TypeVar("T")
 
 
-class PeekProxy(Generic[T]):
-    def __init__(self, own: 'PeekOrdCol[T]', val: T):
-        self.inner: T = val
-        self._owner: 'PeekOrdCol'[T] = own
+class PeekOrdProxy(Generic[T]):
+    def __init__(self, owner: 'PeekOrdCol[T]', val: T):
+        self.val: T = val
+        self._owner: 'PeekOrdCol'[T] = owner
 
     def __repr__(self) -> str:
-        return "PeekProxy(" + str(self.inner) + ")"
+        return "PeekOrdProxy(" + str(self.val) + ")"
 
-    def __lt__(self, other: 'PeekProxy[T]') -> bool:
-        result = self.inner < other.inner
-        entry = None
+    def __lt__(self, other: 'PeekOrdProxy[T]') -> bool:
+        result = self.val < other.val
+        _entry = None
         if result:
-            entry = PeekOrdLogEntry(self, other)
+            _entry = PeekOrdEntry(self, other)
         else:
-            entry = PeekOrdLogEntry(other, self)
-        self._owner.get_or_make_log(other._owner).entries.append(entry)
+            _entry = PeekOrdEntry(other, self)
+        self._owner.get_or_make_log(other._owner).entries.append(_entry)
         return result
 
-    def __gt__(self, other: 'PeekProxy[T]') -> bool:
-        result = self.inner > other.inner
-        entry = None
+    def __gt__(self, other: 'PeekOrdProxy[T]') -> bool:
+        result = self.val > other.val
+        _entry = None
         if result:
-            entry = PeekOrdLogEntry(other, self)
+            _entry = PeekOrdEntry(other, self)
         else:
-            entry = PeekOrdLogEntry(self, other)
-        self._owner.get_or_make_log(other._owner).entries.append(entry)
+            _entry = PeekOrdEntry(self, other)
+        self._owner.get_or_make_log(other._owner).entries.append(_entry)
         return result
 
 
 class PeekOrdCol(Generic[T]):
     def __init__(self, *args: T):
-        self._objs: Set[PeekProxy[T]] = set({PeekProxy(self, val) for val in args})
+        self._objs: Set[PeekOrdProxy[T]] = set({PeekOrdProxy(self, val) for val in args})
         self._logs: Dict[PeekOrdCol[T], PeekOrdLog[T]] = dict()
 
     def __repr__(self) -> str:
         return "PeekOrdCol" + str(self._objs)
 
-    def as_tup(self) -> Tuple[PeekProxy[T]]:
+    def as_tup(self) -> Tuple[PeekOrdProxy[T]]:
         return tuple(self._objs)
 
     def get_or_make_log(self, other: 'PeekOrdCol[T]') -> 'PeekOrdLog[T]':
@@ -55,15 +56,77 @@ class PeekOrdCol(Generic[T]):
 class PeekOrdLog(Generic[T]):
     def __init__(self, list_a: PeekOrdCol[T], list_b: PeekOrdCol[T]):
         self._lists: Tuple[PeekOrdCol[T], PeekOrdCol[T]] = (list_a, list_b)
-        self.entries: List[PeekOrdLogEntry[T]] = list()
+        self.entries: List[PeekOrdEntry[T]] = list()
 
     def __repr__(self) -> str:
         return "PeekOrdLog" + str(self.entries)
 
 
-class PeekOrdLogEntry(Generic[T]):
-    def __init__(self, a: PeekProxy[T], b: PeekProxy[T]):
-        self.lft_lt_rgh: Tuple[PeekProxy[T], PeekProxy[T]] = (a, b)
+class PeekOrdEntry(Generic[T]):
+    def __init__(self, a: PeekOrdProxy[T], b: PeekOrdProxy[T]):
+        self.lft_lt_rgh: Tuple[PeekOrdProxy[T], PeekOrdProxy[T]] = (a, b)
+        self.timestamp: float = time()
+        sleep(0.001)
 
     def __repr__(self) -> str:
-        return "PeekOrdLogEntry(" + str(self.lft_lt_rgh[0]) + " < " + str(self.lft_lt_rgh[1]) + ")"
+        return "PeekOrdEntry(" + str(self.lft_lt_rgh[0]) + " < " + str(self.lft_lt_rgh[1]) + ")"
+
+    def __lt__(self, other: 'PeekOrdEntry[T]') -> bool:
+        return self.timestamp < other.timestamp
+
+    def __gt__(self, other: 'PeekOrdEntry[T]') -> bool:
+        return self.timestamp > other.timestamp
+
+    def __le__(self, other: 'PeekOrdEntry[T]') -> bool:
+        return (self == other) | (self < other)
+
+    def __ge__(self, other: 'PeekOrdEntry[T]') -> bool:
+        return (self == other) | (self > other)
+
+
+class PeekOrdTimeline(Generic[T]):
+    def __init__(self, *logs: PeekOrdLog[T]):
+        entries: List[PeekOrdEntry[T]] = list()
+        for log in logs:
+            entries.extend(log.entries)
+        entries.sort()
+        self._entries: Tuple[PeekOrdEntry[T], ...] = tuple(entries)
+
+    def __getitem__(self, i: SupportsIndex) -> 'PeekOrdFrame[T]':
+        return PeekOrdFrame(self, i.__index__())
+
+    def past(self, i: SupportsIndex) -> Tuple[PeekOrdEntry[T], ...]:
+        return self._entries[0: i.__index__()]
+
+
+class PeekOrdFrame(Generic[T]):
+    def __init__(self, timeline: PeekOrdTimeline[T], index: int):
+        self.timeline: PeekOrdTimeline[T] = timeline
+        self.index: int = index
+
+    def __index__(self) -> int:
+        return self.index
+
+    def check_lt(self, a: PeekOrdProxy[T], b: PeekOrdProxy[T]) -> bool:
+        gt_dict: Dict[PeekOrdProxy[T], Set[PeekOrdProxy[T]]] = dict()
+        for entry in self.timeline.past(self):
+            lhs: PeekOrdProxy[T] = entry.lft_lt_rgh[0]
+            rhs: PeekOrdProxy[T] = entry.lft_lt_rgh[1]
+            if lhs not in gt_dict.keys():
+                gt_dict[lhs] = set()
+            gt_dict[lhs].add(rhs)
+
+        def lt(x: PeekOrdProxy[T], y: PeekOrdProxy[T], cache: Set[PeekOrdProxy[T]] = None) -> bool:
+            if cache is None:
+                cache = set()
+            for gt in gt_dict[x]:
+                if gt in cache:
+                    return False
+                elif gt is y:
+                    return True
+                elif lt(gt, y, cache):
+                    return True
+            cache.add(x)
+            return False
+
+        return lt(a, b)
